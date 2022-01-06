14a31746675f5144b938d8145bf78019d396de41
Jack <Jack@DESKTOP-UPLEJRR>
2022-01-07 08:48:47 +0900

    4

---
+++ och06_aop1/src/main/java/och06_aop1/LogAop.java
@@ -0,0 +1,26 @@
+package och06_aop1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LogAop {
+	// Around Advice에서 사용할 공통기능 메서드는,
+	// 대부분 파라미터로 전달받은 ProceedingJoinPoint의 proceed() 메서드만 호출
+	// Proxy역할을 해서 
+	public Object loggerAop(ProceedingJoinPoint joinpoint) throws Throwable {
+		String signatureStr = joinpoint.getSignature().toString();
+		//핵심 관심사의 실행 메소드
+		System.out.println( signatureStr + " is start. ");
+		long st = System.currentTimeMillis();
+		try { 
+			//핵심관심사를 실행시킴
+			Object obj = joinpoint.proceed();
+			return obj;
+		} finally {
+			long et = System.currentTimeMillis();
+			System.out.println( signatureStr + " is finished.");
+			System.out.println( signatureStr + " 경과시간: " + (et- st));		
+		}
+		/* 각각의 메소드에 대해 성능 측정하는 */
+	}
+
+}
